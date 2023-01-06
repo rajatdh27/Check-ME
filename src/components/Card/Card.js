6e22370431a3881656defc82fc450f9edb84eed0
unionerror <rajatdh27@gmail.com>
2023-01-06 11:08:16 +0530

    added validations

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Card.module.css";
 function Card(props) {
+  const [errorState, setErrorState] = useState({
+    invalid: false,
+    message: "",
+  });
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (
+      props.data.name.trim().length === 0 ||
+      props.data.email.trim().length === 0 ||
+      props.data.userName.trim().length === 0 ||
+      props.data.password.trim().length === 0 ||
+      props.data.confirmPassword.trim().length === 0
+    ) {
+      setErrorState((prevState) => {
+        return {
+          ...prevState,
+          invalid: true,
+          message: "Found empty field",
+        };
+      });
+    } else if (props.data.password !== props.data.confirmPassword) {
+      setErrorState((prevState) => {
+        return {
+          ...prevState,
+          invalid: true,
+          message: "Please recheck password",
+        };
+      });
+    } else {
+      setErrorState((prevState) => {
+        return {
+          ...prevState,
+          invalid: false,
+          message: "",
+        };
+      });
+    }
+  };
   return (
-    <div className={styles.card}>
-      <form className={styles.form}>
-        <div className={styles.title}>
-          <h1>{props.name}</h1>
+    <>
+      {errorState.invalid ? (
+        <div className={styles.invalid}>
+          <h2>{errorState.message}</h2>
         </div>
-        {props.children}
-      </form>
-    </div>
+      ) : (
+        ""
+      )}
+      <div className={styles.card}>
+        <form className={styles.form} onSubmit={submitHandler}>
+          <div className={styles.title}>
+            <h1>{props.name}</h1>
+          </div>
+          {props.children}
+        </form>
+      </div>
+    </>
   );
 }
 
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,21 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./SignUp.module.css";
 import Button from "../Button/Button";
 import Card from "../Card/Card";
 function SignUp() {
+  const [userInput, setUserInput] = useState({
+    name: "",
+    email: "",
+    userName: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const nameHandler = (e) => {
+    setUserInput((prevInput) => {
+      return { ...prevInput, name: e.target.value };
+    });
+  };
+  const emailHandler = (e) => {
+    setUserInput((prevInput) => {
+      return { ...prevInput, email: e.target.value };
+    });
+  };
+  const userNameHandler = (e) => {
+    setUserInput((prevInput) => {
+      return { ...prevInput, userName: e.target.value };
+    });
+  };
+  const passwordHandler = (e) => {
+    setUserInput((prevInput) => {
+      return { ...prevInput, password: e.target.value };
+    });
+  };
+  const confirmPasswordHandler = (e) => {
+    setUserInput((prevInput) => {
+      return { ...prevInput, confirmPassword: e.target.value };
+    });
+  };
   return (
-    <Card name="Sign Up">
+    <Card name="Sign Up" data={userInput}>
       <div className={styles.inputs}>
         <label>Name:</label>
-        <input type="text" name="name" autoComplete="off" />
+        <input
+          type="text"
+          name="name"
+          autoComplete="off"
+          value={userInput.name}
+          onChange={nameHandler}
+        />
         <label>Email:</label>
-        <input type="text" name="password" autoComplete="off" />
+        <input
+          type="email"
+          name="email"
+          autoComplete="off"
+          onChange={emailHandler}
+        />
         <label>User Name:</label>
-        <input type="text" name="password" autoComplete="off" />
+        <input
+          type="text"
+          name="user"
+          autoComplete="off"
+          onChange={userNameHandler}
+        />
         <label>Password:</label>
-        <input type="text" name="password" autoComplete="off" />
+        <input
+          type="password"
+          name="password"
+          autoComplete="off"
+          onChange={passwordHandler}
+        />
+        <span>+</span>
         <label>Confirm Password:</label>
-        <input type="text" name="password" autoComplete="off" />
+        <input
+          type="password"
+          name="password"
+          autoComplete="off"
+          onChange={confirmPasswordHandler}
+        />
       </div>
       <div className={styles.buttonContainer}>
         <div className={styles.button}>
