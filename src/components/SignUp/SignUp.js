bb644a1dfac1a146db59dbe9cff7c90c48bca863
unionerror <rajatdh27@gmail.com>
2023-01-06 12:10:11 +0530

    added toggle for password inputs

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -2,7 +2,17 @@ import React, { useState } from "react";
 import styles from "./SignUp.module.css";
 import Button from "../Button/Button";
 import Card from "../Card/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 function SignUp() {
+  const icons = {
+    open: faEye,
+    close: faEyeSlash,
+  };
+  const [toggle, setToggle] = useState({
+    passwordToggle: false,
+    confirmPasswordToggle: false,
+  });
   const [userInput, setUserInput] = useState({
     name: "",
     email: "",
@@ -35,46 +45,81 @@ function SignUp() {
       return { ...prevInput, confirmPassword: e.target.value };
     });
   };
+  const passwordIconHandler = () => {
+    setToggle((prevState) => {
+      return {
+        ...prevState,
+        passwordToggle: !toggle.passwordToggle,
+      };
+    });
+  };
+  const confirmPasswordIconHandler = () => {
+    setToggle((prevState) => {
+      return {
+        ...prevState,
+        confirmPasswordToggle: !toggle.confirmPasswordToggle,
+      };
+    });
+  };
   return (
     <Card name="Sign Up" data={userInput}>
       <div className={styles.inputs}>
-        <label>Name:</label>
-        <input
-          type="text"
-          name="name"
-          autoComplete="off"
-          value={userInput.name}
-          onChange={nameHandler}
-        />
-        <label>Email:</label>
-        <input
-          type="email"
-          name="email"
-          autoComplete="off"
-          onChange={emailHandler}
-        />
-        <label>User Name:</label>
-        <input
-          type="text"
-          name="user"
-          autoComplete="off"
-          onChange={userNameHandler}
-        />
-        <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          autoComplete="off"
-          onChange={passwordHandler}
-        />
-        <span>+</span>
-        <label>Confirm Password:</label>
-        <input
-          type="password"
-          name="password"
-          autoComplete="off"
-          onChange={confirmPasswordHandler}
-        />
+        <div className={styles.inputContainer}>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            autoComplete="off"
+            value={userInput.name}
+            onChange={nameHandler}
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            autoComplete="off"
+            onChange={emailHandler}
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <label>User Name:</label>
+          <input
+            type="text"
+            name="user"
+            autoComplete="off"
+            onChange={userNameHandler}
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <label>Password:</label>
+          <input
+            type={toggle.passwordToggle ? "text" : "password"}
+            name="password"
+            autoComplete="off"
+            onChange={passwordHandler}
+          />
+          <FontAwesomeIcon
+            icon={toggle.passwordToggle ? icons.close : icons.open}
+            className={styles.icon}
+            onClick={passwordIconHandler}
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <label>Confirm Password:</label>
+          <input
+            type={toggle.confirmPasswordToggle ? "text" : "password"}
+            name="confirmPassword"
+            autoComplete="off"
+            onChange={confirmPasswordHandler}
+          />
+          <FontAwesomeIcon
+            icon={toggle.confirmPasswordToggle ? icons.close : icons.open}
+            className={styles.icon}
+            onClick={confirmPasswordIconHandler}
+          />
+        </div>
       </div>
       <div className={styles.buttonContainer}>
         <div className={styles.button}>
